d253e1ee5d622b407593ece9a580ef47d50e891f
Weronika Kansy <weronika.kansy@gmail.com>
2020-09-15 01:54:40 +0200

    test.js edited; if and else if statements added

--- test.js
+++ test.js
@@ -1,11 +1,19 @@
-var youLikeTrains = true;
+var myAge = 29;
 
-if (youLikeTrains){
+if (myAge > 30){
 
-    document.write("Here are your trains");
+    document.write("you are over 30");
+
+} else if (myAge > 20){
+
+  document.write("you are over 20");
+
+} else if (myAge > 10){
+
+  document.write("you are over 10");
 
 } else {
 
-    document.write("Here are your boats");
+  document.write("you are not over 10");
 
 }
