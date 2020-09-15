46673b00c723f5497b90f1d92f1c38db1998caee
Weronika Kansy <weronika.kansy@gmail.com>
2020-09-15 23:41:13 +0200

    index.html edited; class added to each a tag with for loop

--- test.js
+++ test.js
@@ -1,18 +1,7 @@
-for (i = 0; i < 10; i++){
-  
-  if(i === 5 || i === 3){
+var links = document.getElementsByTagName("a");
 
-    continue;
+for(i = 0; i < links.length; i++){
 
-  }
+links[i].className = "link-" + i;
 
-  console.log(i);
-
-  if(i === 7){
-
-    break;
-
-  }
 }
-
-console.log("I have broken out of the loop");
