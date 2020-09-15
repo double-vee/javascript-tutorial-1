3bef815a8a8021809fdcb86b0a07252e89f52354
Weronika Kansy <weronika.kansy@gmail.com>
2020-09-16 01:44:47 +0200

    test.js edited; if statement with isNaN added

--- test.js
+++ test.js
@@ -1,13 +1,8 @@
-var a = 5;
+var a = "apple";
 var b = 5;
 
-console.log(a + b);
-console.log(typeof (a + b) );
-
-console.log(Math.round(6.5));
-console.log(Math.floor(6.8));
-console.log(Math.ceil(6.2));
-
-console.log(Math.max(6, 4, 8, 10));
-
-console.log(Math.PI);
+if (isNaN(a)) {
+  console.log("this is not a number!");
+} else {
+  console.log("the result is " + (a * b));
+}
