103dfe5be79830eca84f1966365d30f3b0c73376
Weronika Kansy <weronika.kansy@gmail.com>
2020-09-17 00:06:33 +0200

    objects created with constructor function in test.js

--- test.js
+++ test.js
@@ -1,14 +1,18 @@
-var myCar2 = {
-  maxSpeed: 80,
-  driver: "April Ludgate",
-  drive: function(speed, time){
+var Car = function(maxSpeed, driver){
+  this.maxSpeed = maxSpeed;
+  this.driver = driver;
+  this.drive = function(speed, time){
     console.log(speed * time);
-  },
-  logDriver: function(){
+  };
+  this.logDriver = function(){
     console.log("driver name is " + this.driver);
-  }
-};
+  };
+}
 
-myCar2.logDriver();
-console.log(myCar2.maxSpeed);
-myCar2.drive(80, 2);
+var myCar = new Car(40, "Leslie Knope");
+var myCar2 = new Car(50, "Ron Swanson");
+var myCar3 = new Car(60, "Andy Dwyer");
+var myCar4 = new Car(70, "April Ludgate");
+
+myCar.drive(60, 0.5);
+myCar3.logDriver();
