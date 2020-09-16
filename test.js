d9da90af0aec422590c0ebfe9ab3223c633c2a03
Weronika Kansy <weronika.kansy@gmail.com>
2020-09-16 23:29:44 +0200

    function with this keyword created in test.js

--- test.js
+++ test.js
@@ -1,22 +1,14 @@
-var myCar = new Object();
-myCar.maxSpeed = 50;
-myCar.driver = "Ron Swanson";
-
-console.log(myCar.driver);
-
-myCar.drive = function() {
-  console.log("now driving");
-};
-
-myCar.drive();
-
 var myCar2 = {
   maxSpeed: 80,
   driver: "April Ludgate",
   drive: function(speed, time){
-  console.log(speed * time);
+    console.log(speed * time);
+  },
+  logDriver: function(){
+    console.log("driver name is " + this.driver);
   }
 };
 
-console.log(myCar2.driver);
+myCar2.logDriver();
+console.log(myCar2.maxSpeed);
 myCar2.drive(80, 2);
