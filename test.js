52fef4a4adb983299bcdeae281ed0b9171e02150
Weronika Kansy <weronika.kansy@gmail.com>
2020-09-17 22:41:51 +0200

    test.js edited; elements removed and reinserted to DOM with removeChild, appendChild

--- test.js
+++ test.js
@@ -10,3 +10,11 @@ newDiv.appendChild(newA);
 newA.innerHTML = "blog";
 
 links.insertBefore(newDiv, links.getElementsByTagName("a")[0]);
+
+var parent = document.getElementById("header");
+
+var child = parent.getElementsByTagName("div")[0];
+
+var removed = parent.removeChild(child);
+
+parent.appendChild(removed);
