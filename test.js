ae68cc02167299a15328f6ea97a2169f1b374ba4
Weronika Kansy <weronika.kansy@gmail.com>
2020-09-17 23:06:41 +0200

    test.js edited; functions assigned to onclick and onmouseover events

--- test.js
+++ test.js
@@ -1,20 +1,9 @@
-var newDiv = document.createElement("div");
+var title = document.getElementById("page-title");
 
-var newA = document.createElement("a");
+title.onclick = function(){
+  alert("you clicked me");
+};
 
-var links = document.getElementById("header");
-
-links.appendChild(newDiv);
-newDiv.appendChild(newA);
-
-newA.innerHTML = "blog";
-
-links.insertBefore(newDiv, links.getElementsByTagName("a")[0]);
-
-var parent = document.getElementById("header");
-
-var child = parent.getElementsByTagName("div")[0];
-
-var removed = parent.removeChild(child);
-
-parent.appendChild(removed);
+title.onmouseover = function(){
+  alert("you hovered over me");
+};
